4608fe6fc0dfc196f8d0a64eb01d9a4437365c35
kayoz <dawid.kaj09@gmail.com>
2024-11-10 19:09:17 +0100

    added sql config

---
+++ sql_config/db_fill.sql
@@ -0,0 +1,253 @@
+INSERT INTO `flask_api_dev`.`dishes` (`dish_id`, `name`) VALUES
+(1, 'Pierogi ruskie'),
+(2, 'Kotlety schabowe'),
+(3, 'Bigos'),
+(4, 'Żurek'),
+(5, 'Placki ziemniaczane'),
+(6, 'Gołąbki'),
+(7, 'Barszcz czerwony'),
+(8, 'Sałatka jarzynowa'),
+(9, 'Flaki'),
+(10, 'Zupa pomidorowa'),
+(11, 'Rosół'),
+(12, 'Gulasz'),
+(13, 'Sernik'),
+(14, 'Makowiec'),
+(15, 'Kiełbasa biała'),
+(16, 'Kaszanka'),
+(17, 'Racuchy'),
+(18, 'Śledzie w oleju'),
+(19, 'Oscypek'),
+(20, 'Zupa ogórkowa'),
+(21, 'Pączki'),
+(22, 'Kotlet mielony'),
+(23, 'Paprykarz szczeciński'),
+(24, 'Fasolka po bretońsku'),
+(25, 'Knedle ze śliwkami'),
+(26, 'Ryba po grecku'),
+(27, 'Kapuśniak'),
+(28, 'Uszka'),
+(29, 'Zupa grzybowa'),
+(30, 'Krokiety'),
+(31, 'Karp smażony'),
+(32, 'Zupa fasolowa'),
+(33, 'Pasztet'),
+(34, 'Chłodnik litewski'),
+(35, 'Zupa cebulowa'),
+(36, 'Szarlotka'),
+(37, 'Kruche ciastka'),
+(38, 'Czernina'),
+(39, 'Rosołek z kluseczkami'),
+(40, 'Żeberka w miodzie'),
+(41, 'Faworki'),
+(42, 'Kluski śląskie'),
+(43, 'Kaszotto'),
+(44, 'Placki po węgiersku'),
+(45, 'Ryż z jabłkami'),
+(46, 'Zupa dyniowa'),
+(47, 'Makaron z serem'),
+(48, 'Smażone pieczarki'),
+(49, 'Tatar wołowy'),
+(50, 'Pieczeń rzymska');
+
+INSERT INTO `flask_api_dev`.`dishes` (`dish_id`, `name`) VALUES
+(51, 'Hamburger'),
+(52, 'Cheeseburger'),
+(53, 'Hot dog'),
+(54, 'Frytki'),
+(55, 'Pizza Margherita'),
+(56, 'Pizza Pepperoni'),
+(57, 'Pizza hawajska'),
+(58, 'Kebab'),
+(59, 'Nuggetsy z kurczaka'),
+(60, 'Quesadilla'),
+(61, 'Burrito'),
+(62, 'Tacos'),
+(63, 'Wrap z kurczakiem'),
+(64, 'Sałatka Cezar z kurczakiem'),
+(65, 'Kanapka BLT'),
+(66, 'Kanapka z szarpaną wieprzowiną'),
+(67, 'Burger wegetariański'),
+(68, 'Pizza wegetariańska'),
+(69, 'Onion rings (krążki cebulowe)'),
+(70, 'Nachosy z serem'),
+(71, 'Skrzydełka BBQ'),
+(72, 'Sandwich z tuńczykiem'),
+(73, 'Mac & Cheese'),
+(74, 'Tosty z serem i szynką'),
+(75, 'Ziemniaki pieczone z dipem'),
+(76, 'Panini z kurczakiem'),
+(77, 'Poutine (frytki z sosem i serem)'),
+(78, 'Corn dog'),
+(79, 'Churros z czekoladą'),
+(80, 'Falafel w picie'),
+(81, 'Souvlaki'),
+(82, 'Pizza Capricciosa'),
+(83, 'Burger z kurczakiem'),
+(84, 'Wrap warzywny'),
+(85, 'Bagietka z jajkiem i bekonem'),
+(86, 'Kubełek skrzydełek'),
+(87, 'Chicken sandwich'),
+(88, 'Waffle fries (frytki gofrowe)'),
+(89, 'Loaded nachos (nachosy z dodatkami)'),
+(90, 'Tater tots'),
+(91, 'Bagietka z szynką i serem'),
+(92, 'Burger rybny'),
+(93, 'Burger z halloumi'),
+(94, 'Frytki słodkie ziemniaki'),
+(95, 'Gyros w picie'),
+(96, 'Pizza BBQ'),
+(97, 'Sałatka grecka'),
+(98, 'Hot dog chili'),
+(99, 'Szybka sałatka warzywna'),
+(100, 'Frytki belgijskie');
+
+INSERT INTO `flask_api_dev`.`dishes` (`dish_id`, `name`) VALUES
+(101, 'Jajecznica z szynką'),
+(102, 'Owsianka z owocami'),
+(103, 'Płatki śniadaniowe z mlekiem'),
+(104, 'Tosty z awokado'),
+(105, 'Kanapka z jajkiem na twardo'),
+(106, 'Jajka sadzone'),
+(107, 'Omlet z warzywami'),
+(108, 'Jogurt z granolą'),
+(109, 'Budyń waniliowy'),
+(110, 'Serek wiejski z pomidorem'),
+(111, 'Bułka z masłem i dżemem'),
+(112, 'Tosty francuskie'),
+(113, 'Muffin śniadaniowy'),
+(114, 'Placuszki bananowe'),
+(115, 'Kanapka z łososiem i serkiem'),
+(116, 'Smoothie truskawkowe'),
+(117, 'Chleb pełnoziarnisty z masłem orzechowym'),
+(118, 'Pasta jajeczna'),
+(119, 'Kanapka z twarogiem i rzodkiewką'),
+(120, 'Parówki na ciepło'),
+(121, 'Sałatka owocowa'),
+(122, 'Chia pudding z owocami'),
+(123, 'Croissant z masłem'),
+(124, 'Bagietka z camembertem'),
+(125, 'Owsianka z orzechami'),
+(126, 'Kasza manna na mleku'),
+(127, 'Serek homogenizowany'),
+(128, 'Kanapka z mozzarellą i pomidorem'),
+(129, 'Tost z dżemem truskawkowym'),
+(130, 'Owocowy mus jabłkowy'),
+(131, 'Wrap śniadaniowy z warzywami'),
+(132, 'Bagel z łososiem'),
+(133, 'Bułka z kiełkami i awokado'),
+(134, 'Pancakes z syropem klonowym'),
+(135, 'Kanapka z serem żółtym'),
+(136, 'Sałatka z rukolą i fetą'),
+(137, 'Smoothie bowl z granolą'),
+(138, 'Tarta śniadaniowa z warzywami'),
+(139, 'Jajko na miękko'),
+(140, 'Chlebek bananowy'),
+(141, 'Kanapka z hummusem i warzywami'),
+(142, 'Bajgiel z masłem czosnkowym'),
+(143, 'Tosty z pastą z awokado'),
+(144, 'Gofry z owocami'),
+(145, 'Maślanka z owocami leśnymi'),
+(146, 'Kaszka ryżowa z bananem'),
+(147, 'Omlet z łososiem'),
+(148, 'Bagietka z masłem orzechowym'),
+(149, 'Tosty z pastą rybną'),
+(150, 'Smoothie z mango i jogurtem');
+
+INSERT INTO `flask_api_dev`.`users` (`user_id`, `name`, `login`, `password`) VALUES
+(1, 'Marek', 'marekX22', NULL),
+(2, 'Anna', 'annaBee17', NULL),
+(3, 'Piotr', 'piotrekRocks99', NULL),
+(4, 'Katarzyna', 'kaskaStar88', NULL),
+(5, 'Tomasz', 'tomekHero42', NULL),
+(6, 'Agnieszka', 'agaSmile33', NULL),
+(7, 'Paweł', 'pawcio77', NULL),
+(8, 'Magdalena', 'magdaSunshine55', NULL),
+(9, 'Krzysztof', 'krisMaster11', NULL),
+(10, 'Monika', 'moniLove24', NULL);
+
+INSERT INTO `flask_api_dev`.`user_tastes` (`taste_id`, `rating`, `users_user_id`, `dishes_dish_id`) VALUES
+(1, 8, 1, 1),  -- Marek lubi Pierogi ruskie (8/10)
+(2, 7, 1, 2),  -- Kotlety schabowe (7/10)
+(3, 9, 1, 4),  -- Żurek (9/10)
+(4, 5, 1, 5),  -- Placki ziemniaczane (5/10)
+(5, 10, 1, 7), -- Barszcz czerwony (10/10)
+(6, 6, 1, 10), -- Zupa pomidorowa (6/10)
+(7, 4, 1, 13), -- Sernik (4/10)
+(8, 3, 1, 15), -- Kiełbasa biała (3/10)
+(9, 7, 1, 16), -- Kaszanka (7/10)
+(10, 2, 1, 19), -- Oscypek (2/10)
+(11, 8, 1, 21), -- Pączki (8/10)
+(12, 5, 1, 22), -- Kotlet mielony (5/10)
+(13, 9, 1, 26), -- Ryba po grecku (9/10)
+(14, 6, 1, 27), -- Kapuśniak (6/10)
+(15, 7, 1, 29), -- Zupa grzybowa (7/10)
+(16, 8, 1, 30), -- Krokiety (8/10)
+(17, 4, 1, 34), -- Chłodnik litewski (4/10)
+(18, 10, 1, 35), -- Zupa cebulowa (10/10)
+(19, 3, 1, 39), -- Rosołek z kluseczkami (3/10)
+(20, 9, 1, 42); -- Kluski śląskie (9/10)
+
+INSERT INTO `flask_api_dev`.`user_tastes` (`taste_id`, `rating`, `users_user_id`, `dishes_dish_id`) VALUES
+(21, 6, 2, 1),  -- Anna lubi Pierogi ruskie (6/10), inna ocena niż Marek
+(22, 8, 2, 3),  -- Bigos (8/10), potrawa, której Marek nie oceniał
+(23, 10, 2, 4), -- Żurek (10/10), Anna lubi bardziej niż Marek
+(24, 3, 2, 8),  -- Sałatka jarzynowa (3/10), nowa potrawa
+(25, 7, 2, 9),  -- Flaki (7/10), nowa potrawa
+(26, 5, 2, 11), -- Rosół (5/10), nowa potrawa
+(27, 2, 2, 14), -- Makowiec (2/10), nowa potrawa
+(28, 4, 2, 16), -- Kaszanka (4/10), różna ocena
+(29, 9, 2, 17), -- Racuchy (9/10), nowa potrawa
+(30, 6, 2, 18), -- Śledzie w oleju (6/10), nowa potrawa
+(31, 7, 2, 20), -- Zupa ogórkowa (7/10), nowa potrawa
+(32, 3, 2, 23), -- Paprykarz szczeciński (3/10), nowa potrawa
+(33, 8, 2, 25), -- Knedle ze śliwkami (8/10), nowa potrawa
+(34, 5, 2, 28), -- Uszka (5/10), nowa potrawa
+(35, 9, 2, 31), -- Karp smażony (9/10), nowa potrawa
+(36, 4, 2, 32), -- Zupa fasolowa (4/10), nowa potrawa
+(37, 10, 2, 36), -- Szarlotka (10/10), nowa potrawa
+(38, 2, 2, 38), -- Czernina (2/10), nowa potrawa
+(39, 8, 2, 41), -- Faworki (8/10), nowa potrawa
+(40, 6, 2, 42); -- Kluski śląskie (6/10), inna ocena niż Marek
+
+-- Dane dla Marka (user_id = 1)
+INSERT INTO `flask_api_dev`.`user_past_dishes` (`user_past_dish_id`, `eat_time`, `quantity`, `users_user_id`, `dishes_dish_id`) VALUES
+(1, 1730448000, 3, 1, 1),  -- 1 listopada, godz. 8:00, Pierogi ruskie, ilość: 3
+(2, 1730476800, 2, 1, 4),  -- 1 listopada, godz. 16:00, Żurek, ilość: 2
+(3, 1730534400, 5, 1, 5),  -- 2 listopada, godz. 8:00, Placki ziemniaczane, ilość: 5
+(4, 1730563200, 4, 1, 7),  -- 2 listopada, godz. 16:00, Barszcz czerwony, ilość: 4
+(5, 1730620800, 6, 1, 10), -- 3 listopada, godz. 8:00, Zupa pomidorowa, ilość: 6
+(6, 1730649600, 2, 1, 13), -- 3 listopada, godz. 16:00, Sernik, ilość: 2
+(7, 1730707200, 3, 1, 16), -- 4 listopada, godz. 8:00, Kaszanka, ilość: 3
+(8, 1730736000, 4, 1, 19), -- 4 listopada, godz. 16:00, Oscypek, ilość: 4
+(9, 1730793600, 7, 1, 21), -- 5 listopada, godz. 8:00, Pączki, ilość: 7
+(10, 1730822400, 1, 1, 22), -- 5 listopada, godz. 16:00, Kotlet mielony, ilość: 1
+(11, 1730880000, 8, 1, 26), -- 6 listopada, godz. 8:00, Ryba po grecku, ilość: 8
+(12, 1730908800, 2, 1, 27), -- 6 listopada, godz. 16:00, Kapuśniak, ilość: 2
+(13, 1730966400, 3, 1, 29), -- 7 listopada, godz. 8:00, Zupa grzybowa, ilość: 3
+(14, 1730995200, 4, 1, 30), -- 7 listopada, godz. 16:00, Krokiety, ilość: 4
+(15, 1731052800, 2, 1, 34), -- 8 listopada, godz. 8:00, Chłodnik litewski, ilość: 2
+(16, 1731081600, 5, 1, 35), -- 8 listopada, godz. 16:00, Zupa cebulowa, ilość: 5
+(17, 1731139200, 3, 1, 39), -- 9 listopada, godz. 8:00, Rosołek z kluseczkami, ilość: 3
+(18, 1731168000, 2, 1, 42); -- 9 listopada, godz. 16:00, Kluski śląskie, ilość: 2
+
+-- Dane dla Anny (user_id = 2)
+INSERT INTO `flask_api_dev`.`user_past_dishes` (`user_past_dish_id`, `eat_time`, `quantity`, `users_user_id`, `dishes_dish_id`) VALUES
+(19, 1730448000, 4, 2, 1),  -- 1 listopada, godz. 8:00, Pierogi ruskie, ilość: 4
+(20, 1730476800, 6, 2, 3),  -- 1 listopada, godz. 16:00, Bigos, ilość: 6
+(21, 1730534400, 7, 2, 4),  -- 2 listopada, godz. 8:00, Żurek, ilość: 7
+(22, 1730563200, 3, 2, 8),  -- 2 listopada, godz. 16:00, Sałatka jarzynowa, ilość: 3
+(23, 1730620800, 2, 2, 9),  -- 3 listopada, godz. 8:00, Flaki, ilość: 2
+(24, 1730649600, 5, 2, 11), -- 3 listopada, godz. 16:00, Rosół, ilość: 5
+(25, 1730707200, 1, 2, 14), -- 4 listopada, godz. 8:00, Makowiec, ilość: 1
+(26, 1730736000, 2, 2, 16), -- 4 listopada, godz. 16:00, Kaszanka, ilość: 2
+(27, 1730793600, 8, 2, 17), -- 5 listopada, godz. 8:00, Racuchy, ilość: 8
+(28, 1730822400, 3, 2, 18), -- 5 listopada, godz. 16:00, Śledzie w oleju, ilość: 3
+(29, 1730880000, 5, 2, 20), -- 6 listopada, godz. 8:00, Zupa ogórkowa, ilość: 5
+(30, 1730908800, 2, 2, 23), -- 6 listopada, godz. 16:00, Paprykarz szczeciński, ilość: 2
+(31, 1730966400, 6, 2, 25), -- 7 listopada, godz. 8:00, Knedle ze śliwkami, ilość: 6
+(32, 1730995200, 4, 2, 28), -- 7 listopada, godz. 16:00, Uszka, ilość: 4
+(33, 1731052800, 2, 2, 31), -- 8 listopada, godz. 8:00, Karp smażony, ilość: 2
+(34, 1731081600, 7, 2, 32), -- 8 listopada, godz. 16:00, Zupa fasolowa, ilość: 7
+(35, 1731139200, 9, 2, 36), -- 9 listopada, godz. 8:00, Szarlotka, ilość: 9
+(36, 1731168000, 4, 2, 38); -- 9 listopada, godz. 16:00, Czernina, ilość: 4
